f14daf316230037b43d1b6e3e028fe723a42e4b9
mwalter <walter@mpdl.mpg.de>
2023-12-19 15:43:20 +0100

    added form-builder and item-resolver

--- src/app/services/item-resolver.service.ts
+++ src/app/services/item-resolver.service.ts
@@ -1,15 +1,13 @@
 import { inject } from "@angular/core";
 import { ActivatedRouteSnapshot, ResolveFn, Router } from "@angular/router";
 import { of, mergeMap, EMPTY } from "rxjs";
-// TODO Add after MessageServices is added
-// import { MessageService } from "src/app/shared/services/message.service";
+import { MessageService } from "./message.service";
 import { IngeCrudService } from "./inge-crud.service";
 
 export const itemResolver: ResolveFn<any> = (route: ActivatedRouteSnapshot) => {
     const router = inject(Router);
     const service = inject(IngeCrudService);
-    // TODO Add after MessageServices is added
-    // const message = inject(MessageService);
+    const message = inject(MessageService);
     const item_id = route.paramMap.get('id');
     const uri = '/items/' + item_id;
 
@@ -18,8 +16,7 @@ export const itemResolver: ResolveFn<any> = (route: ActivatedRouteSnapshot) => {
             if (item) {
                 return of(item);
             } else {
-                // TODO Add after MessageServices is added
-                // message.warning('Invalid item id');
+                message.warning('Invalid item id');
                 router.navigate(['pure/pure']);
                 return EMPTY;
             }
