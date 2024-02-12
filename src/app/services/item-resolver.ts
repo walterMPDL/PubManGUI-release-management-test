9007f0051c4e19fa7e8558db2104e6d1aa27e784
mwalter <walter@mpdl.mpg.de>
2024-02-12 18:33:30 +0100

    Added item service

--- src/app/services/item-resolver.ts
+++ src/app/services/item-resolver.ts
@@ -2,24 +2,29 @@ import { inject } from "@angular/core";
 import { ActivatedRouteSnapshot, ResolveFn, Router } from "@angular/router";
 import { of, mergeMap, EMPTY } from "rxjs";
 import { MessageService } from "src/app/shared/services/message.service";
-import { IngeCrudService } from "./inge-crud.service";
+import { ItemsService } from "./items.service";
 
 export const itemResolver: ResolveFn<any> = (route: ActivatedRouteSnapshot) => {
     const router = inject(Router);
-    const service = inject(IngeCrudService);
+    const service = inject(ItemsService);
     const message = inject(MessageService);
     const item_id = route.paramMap.get('id');
-    const uri = '/items/' + item_id;
 
-    return service.get(uri, undefined).pipe(
-        mergeMap(item => {
-            if (item) {
-                return of(item);
-            } else {
-                message.warning('Invalid item id');
-                router.navigate(['pure/pure']);
-                return EMPTY;
-            }
-        })
-    );
+    if (item_id == null) {
+        message.warning('Invalid item id');
+        router.navigate(['pure/pure']);
+        return EMPTY
+    } else {
+        return service.getItem(item_id, undefined).pipe(
+            mergeMap(item => {
+                if (item) {
+                    return of(item);
+                } else {
+                    message.warning('Invalid item id');
+                    router.navigate(['pure/pure']);
+                    return EMPTY;
+                }
+            })
+        );
+    }
 }
