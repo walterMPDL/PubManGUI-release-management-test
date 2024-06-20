22a317b7c0f8193a233a513c24e57fdfddda6c83
Markus Haarländer <haarlaender@mpdl.mpg.de>
2024-06-20 16:25:13 +0200

    Header scrolling

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,10 @@
-import { Component, ElementRef, Input, Renderer2, inject } from '@angular/core';
+import {Component, ElementRef, Input, Renderer2, inject, HostListener} from '@angular/core';
 import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
 import { AaComponent } from '../aa/aa.component';
 import { SwitchBsThemeComponent } from 'src/app/shared/components/switch-bs-theme/switch-bs-theme.component';
 import { TooltipDirective } from 'src/app/shared/directives/tooltip.directive';
-import { DOCUMENT } from '@angular/common';
+import {DOCUMENT, NgClass} from '@angular/common';
 import { AaService } from 'src/app/services/aa.service';
 import { LangSwitchComponent } from 'src/app/shared/components/lang-switch/lang-switch.component';
 import { SidenavComponent } from 'src/app/shared/components/sidenav/sidenav.component';
@@ -14,14 +14,14 @@ import { SidenavComponent } from 'src/app/shared/components/sidenav/sidenav.comp
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.scss'],
     standalone: true,
-    imports: [ RouterLink, FormsModule, ReactiveFormsModule, TooltipDirective, SwitchBsThemeComponent, LangSwitchComponent, AaComponent, SidenavComponent]
+  imports: [RouterLink, FormsModule, ReactiveFormsModule, TooltipDirective, SwitchBsThemeComponent, LangSwitchComponent, AaComponent, SidenavComponent, NgClass]
 })
 export class HeaderComponent {
 
   headerHeight: number = 0;
   header!: HTMLElement;
   private document = inject(DOCUMENT);
-  newHeight: any;
+  isScrolled = false;
 
   ngOnInit() {
     const nav = this.document.getElementById('header');
@@ -29,12 +29,6 @@ export class HeaderComponent {
       this.header = nav;
     }
     this.headerHeight = this.header.offsetHeight as number;
-  }
-
-  ngAfterViewInit() {
-    this.document.addEventListener('scroll', (ev) => {
-      this.resizeHeader(ev);
-    });
   }
 
   search_form = this.form_builder.group({
@@ -56,21 +50,11 @@ export class HeaderComponent {
     this.search_form.controls['text'].patchValue('');
   }
 
-  resizeHeader(event: any) {
-    this.newHeight = this.headerHeight - window.scrollY / 2;
-
-    if (this.newHeight < 50) {
-      this.newHeight = 50;
-    }
-
-    let fontsize = this.newHeight / this.headerHeight;
-    if (fontsize >= 0.5) {
-      this.header.style.fontSize = fontsize + 'em';
-    }
-    // if (this.newHeight >= this.headerHeight) {
-      this.header.style.height = this.newHeight + 'px';
-    // }
-
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll() {
+    const scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    this.isScrolled = scrollPosition > 50;
+    console.log(this.isScrolled)
   }
 
   help() {
