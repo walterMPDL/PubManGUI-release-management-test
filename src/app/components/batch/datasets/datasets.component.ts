03cfb6ef3f20af625162e31c125bd8cea69b962c
vilarodr <vilarodriguez@mpdl.mpg.de>
2024-08-19 11:33:53 +0200

    datasets pages to display

--- src/app/components/batch/datasets/datasets.component.ts
+++ src/app/components/batch/datasets/datasets.component.ts
@@ -39,10 +39,11 @@ export default class DatasetsComponent implements OnInit {
   select_pages_2_display = new FormControl(10);
 
   pages_2_display = [
-    { value: 5, label: '5' },
     { value: 10, label: '10' },
-    { value: 20, label: '20' },
+    { value: 25, label: '25' },
     { value: 50, label: '50' },
+    { value: 100, label: '100' },
+    { value: 250, label: '250' },
   ];
 
   // Pagination:
