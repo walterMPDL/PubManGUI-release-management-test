36621a656227e9559a2e16df7b28b8eef03ce0f0
vilarodr <vilarodriguez@mpdl.mpg.de>
2024-10-23 17:36:06 +0200

    Revert "Merge branch 'import'"
    This reverts commit b47bda8ef98466777295113d076c029414994e96, reversing
    changes made to 974e554abd65d1eab603d909e1816feda65299ff.

--- src/app/components/imports/new/fetch/fetch.component.ts
+++ src/app/components/imports/new/fetch/fetch.component.ts
@@ -5,8 +5,6 @@ import { FormBuilder, FormGroup, Validators, FormsModule, ReactiveFormsModule } 
 import { Router } from '@angular/router';
 
 import { ContextDbRO } from 'src/app/model/inge';
-import { ImportsService } from 'src/app/components/imports/services/imports.service';
-import type { GetCrossrefParams, GetArxivParams } from 'src/app/components/imports/interfaces/imports-params';
 import { AaService } from 'src/app/services/aa.service';
 
 @Component({
@@ -19,15 +17,14 @@ import { AaService } from 'src/app/services/aa.service';
   ],
   templateUrl: './fetch.component.html',
 })
-export default class FetchComponent implements OnInit {
+export default class FetchComponent implements OnInit { 
 
   constructor(
-    private fb: FormBuilder,
+    private fb: FormBuilder, 
     private aaSvc: AaService,
-    private importsSvc: ImportsService,
     private router: Router) { }
 
-  user_contexts?: ContextDbRO[] = [];
+  user_contexts?: ContextDbRO[] = [];  
 
   ngOnInit(): void {
     this.aaSvc.principal.subscribe(
@@ -37,30 +34,10 @@ export default class FetchComponent implements OnInit {
     );
   }
 
-  // Initialized for Development ... TO-CLEAR
   public fetchForm: FormGroup = this.fb.group({
-    contextId: ['ctx_persistent3', [Validators.required]],
-    source: ['crossref', [Validators.required]],
-    identifier: ['10.1038%2Fs41586-021-04387-1', [Validators.required]],
-    fullText: ['FULLTEXT_DEFAULT']
+    context: [$localize`:@@batch.actions.context:Context`, [Validators.required]],
+    source: ['1', [Validators.required]]
   });
-
-  get getCrossrefParams(): GetCrossrefParams {
-    const importParams: GetCrossrefParams = {
-      contextId: this.fetchForm.controls['contextId'].value,
-      identifier: this.fetchForm.controls['identifier'].value
-    }
-    return importParams;
-  }
-
-  get getArxivParams(): GetArxivParams {
-    const importParams: GetArxivParams = {
-      contextId: this.fetchForm.controls['contextId'].value,
-      identifier: this.fetchForm.controls['identifier'].value,
-      fullText: this.fetchForm.controls['fullText'].value
-    }
-    return importParams;
-  }
 
   onSubmit(): void {
     if (this.fetchForm.invalid) {
@@ -68,19 +45,7 @@ export default class FetchComponent implements OnInit {
       return;
     }
 
-    const source = this.fetchForm.controls['source'].value;
-
-    switch (source) {
-      case 'crossref':
-        this.importsSvc.getCrossref(this.getCrossrefParams).subscribe(importResponse => {
-          this.router.navigateByUrl('/edit_import');
-        });
-        break;
-      case 'arxiv':
-        this.importsSvc.getArxiv(this.getArxivParams).subscribe(importResponse => {
-          this.router.navigateByUrl('/edit_import');
-        });
-    }
+    this.router.navigate(['/import']);
   }
 
 }
