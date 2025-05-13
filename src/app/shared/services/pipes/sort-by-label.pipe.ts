9c879e8f7b631ef1d4249ee2e9d9232f2a93e4a2
haarli <haarlaender@mpdl.mpg.de>
2025-05-13 12:05:11 +0200

    sort of genre list by translation

---
+++ src/app/shared/services/pipes/sort-by-label.pipe.ts
@@ -0,0 +1,37 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import {KeyValuePipe} from "@angular/common";
+import {TranslateService} from "@ngx-translate/core";
+
+const keepOrder = (a: any, b: any) => a;
+
+@Pipe({
+  name: 'sortByLabel',
+  standalone: true
+})
+export class SortByLabelPipe implements PipeTransform {
+
+  constructor(private translateService: TranslateService) {
+  }
+
+  transform(value: string[] | undefined, ...args: any[]): any {
+
+    console.log("Sort by label")
+    let prefix = "";
+    if(args && args.length > 0) {
+      prefix = args[0]
+    }
+    if(value) {
+      const translatedMap = value.map(val => {
+        return {"key" : val, "value" : this.translateService.instant(prefix + val)};
+      })
+      //sort by translation
+      translatedMap.sort((a,b) => a.value.localeCompare(b.value));
+
+      //return keys
+      return translatedMap.map(o => o.key);
+    }
+
+    return value;
+  }
+
+}
