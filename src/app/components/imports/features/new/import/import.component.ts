f780f47e9c9a78421b5e9dfb98876e4866de3cf5
vilarodr <vilarodriguez@mpdl.mpg.de>
2025-05-14 14:06:35 +0200

    _STRING cleaned from format options

--- src/app/components/imports/features/new/import/import.component.ts
+++ src/app/components/imports/features/new/import/import.component.ts
@@ -46,6 +46,8 @@ export default class ImportComponent implements OnInit {
   importFormat = Object.keys(ImportFormat);
   user_contexts?: ContextDbRO[] = [];
 
+  REGEXP=/_STRING/gi
+  
   public importForm: FormGroup = this.fb.group({
     contextId: [this.translateService.instant(_('imports.context')), [Validators.required]],
     importName: ['', [Validators.required]],
