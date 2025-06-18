68d6af8c1e9b946fe6b7a19f1fdbd879bb58729f
vilarodr <vilarodriguez@mpdl.mpg.de>
2025-06-18 15:09:26 +0200

    messages and footer responsive

--- src/app/shared/components/sidenav/sidenav.component.ts
+++ src/app/shared/components/sidenav/sidenav.component.ts
@@ -1,5 +1,5 @@
-import { CommonModule } from '@angular/common';
-import { AfterViewInit, Component, ElementRef, Input, OnInit, Renderer2, ViewChild, inject } from '@angular/core';
+import { CommonModule, DOCUMENT } from '@angular/common';
+import { AfterViewInit, Component, ElementRef, Input, Renderer2, ViewChild, inject, OnChanges, SimpleChanges, DoCheck } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { AaService } from "../../../services/aa.service";
 
@@ -26,18 +26,25 @@ import { LangSwitchComponent } from 'src/app/shared/components/lang-switch/lang-
 export class SidenavComponent implements AfterViewInit {
 
   @Input() mobile !: boolean;
+  private lastSight: boolean = false;
 
   @ViewChild('sidenav', { read: ElementRef }) nav!: ElementRef;
   renderer = inject(Renderer2);
+  content: HTMLElement | null = null;
+  messaging: HTMLElement | null = null;
+  nav_mobile: boolean = false;
 
   aaService = inject(AaService);
   batchSvc = inject(BatchService);
   importsSvc = inject(ImportsService);
   cartService = inject(CartService);
+  private document = inject(DOCUMENT);
 
   ngAfterViewInit(): void {
     this.collapse();
     this.batchSvc.items;
+    this.content = this.document.getElementById('content');
+    this.messaging = this.document.getElementById('messaging');
   }
 
   /*
@@ -51,11 +58,32 @@ export class SidenavComponent implements AfterViewInit {
     if (this.aaService.principal.getValue().isDepositor || this.aaService.principal.getValue().isModerator) {
       if (!this.importsSvc.hasImports()) this.importsSvc.checkImports();
     }
-    this.renderer.removeClass(this.nav.nativeElement, 'collapsed');
+
+    if (this.mobile) {
+      this.nav_mobile = !this.nav_mobile;
+      if (this.content) {
+        if (this.nav_mobile) {
+          this.content.style.marginTop = '30.5em';
+          this.messaging!.style.marginTop = '31.5em';
+        }
+        else {
+          this.content.style.marginTop = '0';
+          this.messaging!.style.marginTop = '0';
+        }
+      }
+    } else {
+      this.renderer.removeClass(this.nav.nativeElement, 'collapsed');
+    }
   }
 
   collapse() {
-    this.renderer.addClass(this.nav.nativeElement, 'collapsed');
+    if (!this.mobile) {
+      this.renderer.addClass(this.nav.nativeElement, 'collapsed');
+      if (this.content) {
+        this.content.style.marginTop = '0';
+        this.messaging!.style.marginTop = '0';
+      }
+    }
   }
 
 }
