bf86799c6b23ab05c834a5656f6a4aa4ba5f1e4b
mwalter <walter@mpdl.mpg.de>
2025-07-22 14:57:21 +0200

    removed selector from creator-form and a small bugfix for creator validation

--- src/app/directives/validation/creator-validation.directive.ts
+++ src/app/directives/validation/creator-validation.directive.ts
@@ -9,25 +9,26 @@ export const creatorValidator: ValidatorFn = (control: AbstractControl,): Valida
   const error_types = Errors;
   const creator = control;
   let currentErrors = {} as ValidationErrors;
-  if (creator !== null ) {
+  if (creator !== null) {
     switch (creator.get('type')?.value) {
       case CreatorType.ORGANIZATION:
         const organization = creator.get('organization');
         if (organization !== null) {
-          if (organization.get('name')?.value ||
-            organization.get('adress')?.value) {
+          if ((organization.get('name')?.value ||
+            organization.get('adress')?.value)
+            && creator.get('role')?.value === null) {
             currentErrors[error_types.CREATOR_ROLE_NOT_PROVIDED] = true;
           }
           if (!organization.get('name')?.value) {
-              currentErrors[error_types.CREATOR_ORGANIZATION_NAME_NOT_PROVIDED] = true;
-            }
+            currentErrors[error_types.CREATOR_ORGANIZATION_NAME_NOT_PROVIDED] = true;
+          }
         }
         break;
       case CreatorType.PERSON:
         const person = creator.get('person');
         if (person !== null) {
           if (!person.get('familyName')?.value) {
-            currentErrors[error_types.CREATOR_FAMILY_NAME_NOT_PROVIDED] =  true;
+            currentErrors[error_types.CREATOR_FAMILY_NAME_NOT_PROVIDED] = true;
           }
           if ((person.get('familyName')?.value ||
             person.get('givenName')?.value) && creator.get('role')?.value === null) {
@@ -46,8 +47,9 @@ export const creatorValidator: ValidatorFn = (control: AbstractControl,): Valida
             let j = 1;
             for (const organization of personOrganizations.controls) {
               if (organization !== null) {
-                if ((organization.get('name')?.value ||
-                  organization.get('adress')?.value) && creator.get('role')?.value === null ) {
+                if ((organization.get('name')?.value
+                  || organization.get('adress')?.value)
+                  && creator.get('role')?.value === null) {
                   currentErrors[error_types.CREATOR_ROLE_NOT_PROVIDED] = true;
                 } // if
                 else if (organization.get('name')?.value === null &&
