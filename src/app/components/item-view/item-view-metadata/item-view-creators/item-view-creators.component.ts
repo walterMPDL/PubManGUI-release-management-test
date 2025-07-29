fa8f65e27aba7cca84d2bc6f423da001ab9a13d5
haarli <haarlaender@mpdl.mpg.de>
2025-07-29 16:28:53 +0200

    info icon for ou autosuggest

--- src/app/components/item-view/item-view-metadata/item-view-creators/item-view-creators.component.ts
+++ src/app/components/item-view/item-view-metadata/item-view-creators/item-view-creators.component.ts
@@ -7,6 +7,7 @@ import { OrganizationsService } from "../../../../services/pubman-rest-client/or
 import { EmptyPipe } from "../../../../pipes/empty.pipe";
 import { TranslatePipe } from "@ngx-translate/core";
 import { LoadingComponent } from "../../../shared/loading/loading.component";
+import { OuModalComponent } from "../../../shared/ou-modal/ou-modal.component";
 
 @Component({
   selector: 'pure-item-view-creators',
@@ -119,14 +120,19 @@ export class ItemViewCreatorsComponent {
     this.maxDisplay = 20;
   }
 
-  toggleAffPopover(content: TemplateRef<any>, aff: OrganizationVO) {
+  toggleAffPopover(ou: OrganizationVO) {
+
 
       this.selectedAffiliationForPopover = undefined;
-      this.modalService.open(content, {size: 'lg'});
+      const componentInstance = this.modalService.open(OuModalComponent, {size: 'lg'}).componentInstance;
+      componentInstance.ouId = ou.identifier;
+      /*
       this.ouService.retrieve(aff.identifier).subscribe(ou => {
         this.selectedAffiliationForPopover = ou;
 
       })
+
+       */
 
     }
 }
