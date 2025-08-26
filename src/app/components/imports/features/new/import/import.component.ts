ab1401f1138f7543dcb2ccf57d62cecc87ebe1db
vilarodr <vilarodriguez@mpdl.mpg.de>
2025-08-26 11:42:41 +0200

    INGUI-70 Deletion of a completed import form

--- src/app/components/imports/features/new/import/import.component.ts
+++ src/app/components/imports/features/new/import/import.component.ts
@@ -196,7 +196,7 @@ export default class ImportComponent implements OnInit {
 
   @HostListener('document:click', ['$event'])
   clickOutside(event: Event) {
-    if (!this.elRef.nativeElement.contains(event.target)) {
+    if (this.elRef.nativeElement.parentElement.contains(event.target) && !this.elRef.nativeElement.contains(event.target)) {
       this.importForm.reset();
 
       this.formatObject = null;
