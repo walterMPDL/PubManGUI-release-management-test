4a321acd3d303883a553ed56c39ff80d65dcac6d
haarli <haarlaender@mpdl.mpg.de>
2025-09-24 14:30:42 +0200

    Refactored HttpInterceptor

--- src/app/services/interceptors/with-credentials.interceptor.ts
+++ src/app/services/interceptors/with-credentials.interceptor.ts
@@ -1,19 +1,13 @@
 import { Injectable } from '@angular/core';
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { HttpEvent, HttpHandler, HttpHandlerFn, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-@Injectable()
-export class WithCredentialsInterceptor implements HttpInterceptor {
 
-  constructor(
-    //private aa: AaService
-  ) { }
-
-  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+  export function withCredentialsHttpInterceptor(request: HttpRequest<unknown>, next: HttpHandlerFn): Observable<HttpEvent<unknown>> {
     //const token = this.aa.token;
     //if (this.aa.principal.getValue()?.loggedIn) {
       request = request.clone({ withCredentials: true });
     //}
-    return next.handle(request);
+    return next(request);
   }
-}
+
