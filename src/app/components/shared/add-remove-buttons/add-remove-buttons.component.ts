c767446c13bc264d34d8447ebb61ae3746b9dc13
haarli <haarlaender@mpdl.mpg.de>
2025-09-26 15:03:09 +0200

    add-remove-buttons change

--- src/app/components/shared/add-remove-buttons/add-remove-buttons.component.ts
+++ src/app/components/shared/add-remove-buttons/add-remove-buttons.component.ts
@@ -9,26 +9,30 @@ import { TranslateService } from "@ngx-translate/core";
   templateUrl: './add-remove-buttons.component.html',
   styleUrls: ['./add-remove-buttons.component.scss']
 })
+
+
 export class AddRemoveButtonsComponent implements OnInit {
 
   @Input({ required: true }) index!: number;
   @Input() add_val?: string;
   @Input() del_val?: string;
+  @Input() addIconAndText?: boolean = false;
   @Input() hiddenAddButton?: boolean;
   @Input() disableAdd: boolean = false;
   @Input() disableRemove: boolean = false;
   @Output() notice = new EventEmitter();
-  add_value: string | undefined;
-  del_value: string | undefined;
 
   constructor(private translateService: TranslateService) { }
   ngOnInit() {
+    /*
     this.add_value = this.add_val ? this.add_val : '<i class="bi bi-plus"></i>';
-    if(this.add_val && this.add_val === this.translateService.instant("common.add")) {
+    if(this.add_val && this.addIconAndText) {
       //this.add_value = '<span class="material-symbols-outlined">' + this.add_symbol + '</span> ' + this.add_value;
-      this.add_value = '<i class="bi bi-plus-lg"></i> ' + this.add_value;
+      this.add_value = '<i class="bi bi-plus-lg"></i> ' + this.add_val;
     }
     this.del_value = this.del_val ? this.del_val : '<i class="bi bi-dash"></i>';
+
+     */
   }
 
   add(i: number) {
