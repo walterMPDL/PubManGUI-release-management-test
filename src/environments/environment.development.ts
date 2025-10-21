2992e07c4821310e8fc334fbe8a719a24d477290
haarli <haarlaender@mpdl.mpg.de>
2025-10-21 14:27:40 +0200

    do not use proxy for dev environment anymore

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
   cone_instance_uri: "http://localhost:4200/cone",
-  inge_rest_uri: "http://localhost:4200/rest",
-  inge_uri: "http://localhost:4200",
+  inge_rest_uri: "https://gui.inge.mpdl.mpg.de/rest",
+  inge_uri: "https://gui.inge.mpdl.mpg.de",
   pure_blog_feed_url: "http://localhost:4200/pureblogfeed"
 };
 
