ae9a07aaafc6d476fb23a7d6c2f3d1e52e030c1a
haarli <haarlaender@mpdl.mpg.de>
2025-10-23 09:04:47 +0200

    Added first matomo functionality

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,8 @@ export const environment = {
   cone_instance_uri: "https://gui.inge.mpdl.mpg.de/cone",
   inge_rest_uri: "https://gui.inge.mpdl.mpg.de/rest",
   inge_uri: "https://gui.inge.mpdl.mpg.de",
-  pure_blog_feed_url: "https://blog.pure.mpg.de/json1"
+  pure_blog_feed_url: "https://blog.pure.mpg.de/json1",
+  matomo_enabled: true,
+  matomo_site_url: "https://analytics.mpdl.mpg.de",
+  matomo_site_id: "91"
 };
